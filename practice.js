b54385e5cdb1fcfdf951c372f3640eb13396ff18
Taru Wiik <taruwiik@gmail.com>
2025-02-03 20:58:44 +0200

    added functions to practice higher order functions and passing functions as arguments

--- practice.js
+++ practice.js
@@ -138,3 +138,31 @@ function fibonacciGenerator(arraySize) {
 }
 
 //console.log(fibonacciGenerator(15));
+
+//Higher order functions & Passing functions as arguments
+//Calculator
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function substract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+function calculator(num1, num2, operator) {
+  return operator(num1, num2);
+}
+
+//console.log(calculator(6, 3, add));
+//console.log(calculator(6, 3, substract));
+//console.log(calculator(6, 3, multiply));
+//console.log(calculator(6, 3, divide));
